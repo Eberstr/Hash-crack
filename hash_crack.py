372b86dc500c255a7ef2c90fc848196644af1e5e
eberstr <eber.antonio9298@gmail.com>
2024-11-13 23:10:54 -0600

    Draft: Hilos para leer lineas y crear hashes

--- hash_crack.py
+++ hash_crack.py
@@ -2,47 +2,67 @@
 
 import hashlib
 import argparse
+import multiprocessing
 
 def get_arguments():
     parser = argparse.ArgumentParser(description="Hash cracker")
     parser.add_argument('-w', '--wordlist', dest='wordlist', help='Ruta del archivo wordlist', required=True)
-    parser.add_argument('--hash', dest='hash', help='Hash to break', required=True)
+    parser.add_argument('-H', dest='hash_value', help='Hash to break', required=True)
     parser.add_argument('-a', dest='algorithm', help='Algoritmo de hasheo', required=True)
     parser.add_argument('-t', dest='threads', help='Number of threads to use')
 
     return parser.parse_args()
 
+# Eliminar esta funcion?????
 def count_lines():
     count=0
     with open(wordlist, errors="ignore") as file:
         lines = file.readlines()
         for line in lines:
-            count += 1
-        return count
+            lines_number += 1
+        return lines_number
 
-def parse_wordlist():
-    lines = count_lines()
-    files = []
-    if lines >= 1000:
-        for i in range(1, lines+1):
-            with open(f'file_{1}.txt', 'x') as f:
+def parse_wordlist(wordlist):
+    lines_number = count_lines()
+	    block = 1000
+	    with open(wordlist, block) as file:
+		    lines = []
+            for line in file:
+                lines.append(line)
+                if len(lines) == block:
+                   yield lines
+                   lines = []
+                if lines:
+                    yield lines
                 
-
-
-def hash_chek(wordlist, algorithm, hash):
-    with open(wordlist) as wl:
-        for word in wl:
-            h = hashlib.new(algorithm)
-            h.update(word.strip().encode('utf-8'))
-            if h.hexdigest() == hash.lower():
-                break
-    return word
+def hash_chek(lines, algorithm, hash_value):
+    for line in lines:
+        h = hashlib.new(hash_value)
+        h.update(line.strip().encode("utf8"))
+        if h.hexdigest() == hash_value.lower():
+            return line
+            break
+    
+    return none
 
 def main():
     args = get_arguments()
 
-    password = hash_chek(args.wordlist, args.algorithm, args.hash)
-    print(password)
+    processes = []
+    threads = int(args.threads) if args.threads else: multiprocessing.cpu_count()
+
+    for block in parse_wordlist(args.wordlist):
+        process = multiprocessing.Process(target=hash_check, block)
+        process.start()
+        processes.appen(process)
+
+        if len(processes) >= threads:
+            for p in processes:
+                p.join()
+            processes = []
+
+    for p in processes:
+        p.join()
 
 if __name__ == '__main__':
     main()
